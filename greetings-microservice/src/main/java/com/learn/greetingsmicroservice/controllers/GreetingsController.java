65693a99f4b9c245a712b6240b56d5ec87104e3e
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2022-09-25 22:53:30 +0530

    Two Methods Tested

--- greetings-microservice/src/main/java/com/learn/greetingsmicroservice/controllers/GreetingsController.java
+++ greetings-microservice/src/main/java/com/learn/greetingsmicroservice/controllers/GreetingsController.java
@@ -5,12 +5,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/greetings")
 public class GreetingsController {
 
+    // http://localhost:8081/api/v1/greetings
     @GetMapping("")
     public ResponseEntity<GreetingsResponseDto> welcome(){
 
@@ -20,4 +22,16 @@ public class GreetingsController {
         ResponseEntity<GreetingsResponseDto> responseEntity = new ResponseEntity<>(greetingsResponseDto, HttpStatus.OK);
         return responseEntity;
     }
+
+    // http://localhost:8081/api/v1/greetings/greet?userName=SriVaru
+    @GetMapping("/greet")
+    public ResponseEntity<GreetingsResponseDto> greetUser(@RequestParam("userName") String userName){
+
+        GreetingsResponseDto greetingsResponseDto = new GreetingsResponseDto();
+        greetingsResponseDto.setMessage(userName + ", Welcome to the Spring Boot world of Azure Containers!");
+
+        ResponseEntity<GreetingsResponseDto> responseEntity = new ResponseEntity<>(greetingsResponseDto, HttpStatus.OK);
+        return responseEntity;
+    }
+
 }
--- greetings-microservice/src/main/java/com/learn/greetingsmicroservice/dtos/GreetingsResponseDto.java
+++ greetings-microservice/src/main/java/com/learn/greetingsmicroservice/dtos/GreetingsResponseDto.java
@@ -8,6 +8,6 @@ import lombok.Setter;
 public class GreetingsResponseDto {
 
     private boolean success = true;
-    
+
     private String message;
 }
