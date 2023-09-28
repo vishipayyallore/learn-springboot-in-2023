00ed308d2499ee6b99518e3a85f0869f5f63b87b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-28 18:05:17 +0530

    Initial Version

---
+++ awsccdemo/src/main/java/com/sv/awsccdemo/awsccdemo/AwsccdemoApplication.java
@@ -0,0 +1,13 @@
+package com.sv.awsccdemo.awsccdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsccdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsccdemoApplication.class, args);
+	}
+
+}
