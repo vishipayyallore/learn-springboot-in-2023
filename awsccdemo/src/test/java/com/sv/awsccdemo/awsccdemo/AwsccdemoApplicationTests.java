e337e7878fb96c0054fef9650ffe513cc89ecea6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-29 08:19:48 +0530

    Swamy/28sep simple api demo (#4)
    * Initial Version
    
    * Initial Version
    
    * Update GreetingsController.java

---
+++ awsccdemo/src/test/java/com/sv/awsccdemo/awsccdemo/AwsccdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sv.awsccdemo.awsccdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsccdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
